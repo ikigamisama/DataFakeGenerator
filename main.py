afa21c80ee7adc1cfba03e2a3f24febbc175620e
Ikigami Sama <ikigamidevs.15@gmail.com>
2025-10-18 22:55:40 +0800

    Updates

--- main.py
+++ main.py
@@ -4,40 +4,10 @@ from core.exporters import Exporter
 
 schema = [
     {
-        "label": "Bitcoin Address",
-        "key_label": "bitcoin_address",
-        "group": 'crypto',
+        "label": "Catch Praise",
+        "key_label": "catch_praise",
+        "group": 'personal',
         "options": {"blank_percentage": 0.1}
-    },
-    {
-        "label": "Binomial Distribution",
-        "key_label": "binomial_distribution",
-        "group": 'basic',
-        "options": {"blank_percentage": 0.1, 'probability': 0.5}
-    },
-    {
-        "label": "Exponential Distribution",
-        "key_label": "exponential_distribution",
-        "group": 'basic',
-        "options": {"blank_percentage": 0.1, 'lam': 1}
-    },
-    {
-        "label": "Geometric Distribution",
-        "key_label": "geometric_distribution",
-        "group": 'basic',
-        "options": {"blank_percentage": 0.1, 'probability': 0.5}
-    },
-    {
-        "label": "Normal Distribution",
-        "key_label": "normal_distribution",
-        "group": 'basic',
-        "options": {"blank_percentage": 0.1, 'mean': 0, 'std_dev': 1, 'decimals': 2}
-    },
-    {
-        "label": "Poisson Distribution",
-        "key_label": "poisson_distribution",
-        "group": 'basic',
-        "options": {"blank_percentage": 0.1, 'mean': 0.5}
     },
 ]
 
